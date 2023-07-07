ee9c67c2fab5e0df4c9c70798d057d897dfed462
Dhanush <dhanushs1357@gmail.com>
2023-07-07 13:31:22 +0530

    Completed with few faults

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
-import { StyleSheet, View, Text, ScrollView, Animated, TouchableOpacity, Dimensions, Platform } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Animated, TouchableOpacity, Platform, Dimensions } from "react-native";
 import * as FileSystem from "expo-file-system";
 import { shareAsync } from "expo-sharing";
 import { FontAwesome } from "@expo/vector-icons";
 import Cbutton from "./components/floatingButton";
-import FAB from "./components/chargerDetails";
 import * as Location from "expo-location";
 import { api } from "./axios/constants";
 import axios from "axios";
@@ -40,12 +39,58 @@ export default function App() {
 
 	//Markers of chargers location
 	const data = require("./chargerData.json");
-	const showLocationsOfInterest = () => {
+	const showLocationsOfMarker = () => {
 		return data.chargers.map((item, index) => {
 			return (
 				<Marker key={index} coordinate={item.location} title={item.name}>
 					<FontAwesome size={30} name="map-pin" fade style={{ color: "#ff2424" }} />
 				</Marker>
+			);
+		});
+	};
+
+	const supportedConnectors = () => {
+		return data.chargers.map((item, index) => {
+			return (
+				<Marker key={index} coordinate={item.location} title={item.name}>
+					<FontAwesome size={30} name="map-pin" fade style={{ color: "#ff2424" }} />
+				</Marker>
+			);
+		});
+	};
+
+	//CardView of markers
+	const showChargerDetails = () => {
+		return data.chargers.map((item, index) => {
+			return (
+				<Animated.View style={styles.card} key={index}>
+					<Text style={styles.cardTitle} numberOfLines={1}>
+						{item.name}
+					</Text>
+					<Text style={styles.cardAddress}>
+						{item.address}
+						<Text>{"                              "}</Text>
+						<Text style={{ color: "#DA396C" }}>
+							{item.distance} {item.distance_metrics}
+						</Text>
+					</Text>
+					<Text style={styles.supCon}>supported connectors</Text>
+					<Text style={styles.avaCOn}>
+						{item.connector_types[0]}
+						{"\n"}
+						<Text style={styles.chSpeed}>15kW Fast Charging</Text>
+					</Text>
+					<Text style={styles.avaCOn}>
+						{item.connector_types[1]}
+						{"\n"}
+						<Text style={styles.chSpeed}>50kW Fast Charging</Text>
+					</Text>
+					<Text style={styles.avaCOn}>
+						{item.connector_types[2]}
+						{"\n"}
+						<Text style={styles.chSpeed}>3kW Fast Charging</Text>
+					</Text>
+				</Animated.View>
 			);
 		});
 	};
@@ -84,12 +129,15 @@ export default function App() {
 	return (
 		<View style={styles.mainContainer}>
 			<MapView ref={mapRef} style={styles.map} region={mapRegion}>
-				{showLocationsOfInterest()}
+				{showLocationsOfMarker()}
 				<Marker coordinate={mapRegion}>
 					<FontAwesome size={30} name="location-arrow" beatFade style={{ color: "#da36dd" }} />
 				</Marker>
 			</MapView>
 			<Cbutton onPress={takeSnapshotAndShare} title="Sharee"></Cbutton>
+			<Animated.ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.scrollView}>
+				{showChargerDetails()}
+			</Animated.ScrollView>
 		</View>
 	);
 }
@@ -98,6 +146,57 @@ export default function App() {
 const styles = StyleSheet.create({
 	mainContainer: {
 		flex: 1,
+	},
+	chSpeed: {
+		color: "#3BAE96",
+		fontSize: 10,
+	},
+	avaCOn: {
+		marginVertical: 5,
+		marginHorizontal: 20,
+		color: "#fff",
+		textTransform: "capitalize",
+	},
+	supCon: {
+		marginVertical: 10,
+		marginHorizontal: 10,
+		color: "#3BAE96",
+		fontSize: 12,
+		fontWeight: "bold",
+		textTransform: "uppercase",
+	},
+	card: {
+		elevation: 2,
+		height: 225,
+		width: 200,
+		backgroundColor: "#191919",
+		borderRadius: 10,
+		marginHorizontal: 5,
+		overflow: "hidden",
+	},
+	cardTitle: {
+		paddingTop: 15,
+		paddingLeft: 10,
+		fontSize: 12,
+		color: "#fff",
+		fontWeight: "bold",
+		textTransform: "uppercase",
+	},
+	cardAddress: {
+		fontSize: 10,
+		paddingLeft: 10,
+		paddingTop: 2,
+		color: "#8a8a91",
+		fontWeight: "bold",
+	},
+
+	scrollView: {
+		position: "absolute",
+		bottom: 0,
+		left: 0,
+		right: 0,
+		paddingVertical: 30,
+		paddingHorizontal: 5,
 	},
 	container: {
 		flex: 1,
